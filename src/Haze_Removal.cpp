d091d19410208c7aec1b87c01cddd41871505110
HaiPh <haiphamnguyenhoang@gmail.com>
2022-04-10 14:51:41 +0800

    update main()

--- src/Haze_Removal.cpp
+++ src/Haze_Removal.cpp
@@ -9,23 +9,24 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char **args) {
-        const char * img_path = args[1];
-        Mat in_img = imread("images/Haze_Singapore.jpg", IMREAD_UNCHANGED);
-        Mat out_img(in_img.rows, in_img.cols, CV_8UC3);
-        unsigned char * indata = in_img.data;
-        unsigned char * outdata = out_img.data;
+  const char * img_path = args[1];
+  Mat in_img;
+  if (args[1])
+    in_img = imread(img_path, IMREAD_UNCHANGED);
+  else
+    in_img = imread("images/Haze_Singapore.jpg", IMREAD_UNCHANGED);
+  Mat out_img(in_img.rows, in_img.cols, CV_8UC3);
+  unsigned char * indata = in_img.data;
+  unsigned char * outdata = out_img.data;
 
-        CHazeRemoval hr;
-        cout << hr.InitProc(in_img.cols, in_img.rows, in_img.channels()) << endl;
-        //auto start = std::chrono::high_resolution_clock::now();
-        cout << hr.Process(indata, outdata, in_img.cols, in_img.rows, in_img.channels()) << endl;
-        //auto stop = std::chrono::high_resolution_clock::now();
-        //float duration = std::chrono::duration_cast<microseconds>(stop - start).count();
-        //cout << "Processing time: " << to_string(duration) << " us ~ " << to_string(1/(duration/1000000)) << " FPS" << endl;
-        //cout << "Expected       : 16667 us ~ 60 FPS" << endl;
-        namedWindow("out_img", WINDOW_AUTOSIZE);
-        imshow("out_img", out_img);
-        waitKey(0);
-        // system("pause");
-        return 0;
+  CHazeRemoval hr;
+  cout << hr.InitProc(in_img.cols, in_img.rows, in_img.channels()) << endl;
+  cout << hr.Process(indata, outdata, in_img.cols, in_img.rows, in_img.channels()) << endl;
+
+  Mat dst;
+  cv::hconcat(in_img, out_img, dst);
+  namedWindow("IDCP", WINDOW_AUTOSIZE);
+  imshow("IDCP", dst);
+  waitKey(0);
+  return 0;
 }
